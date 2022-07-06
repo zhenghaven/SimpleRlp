c1bd8fc9322b2196fdbd8d572d4a38b8375873a4
Haofan Zheng <hzheng6@ucsc.edu>
2022-07-06 16:47:57 -0700

    Added CAT string in AdvRlp

--- include/AdvancedRlp/CatString.hpp
+++ include/AdvancedRlp/CatString.hpp
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "Internal/SimpleUtf.hpp"
+
 #include "ParserUtils.hpp"
 
 #ifndef ADVANCEDRLP_CUSTOMIZED_NAMESPACE
@@ -19,8 +21,201 @@ namespace ADVANCEDRLP_CUSTOMIZED_NAMESPACE
 // Parsers
 // ====================
 
+
+template<typename _ListObjType>
+inline void PreCheckCatString(size_t pos, const _ListObjType& l)
+{
+	using namespace Internal::SimRlp::Internal::Obj;
+	Internal::CheckRlpListTypeSizeEq("CAT String", pos, l,
+		std::pair<std::string, ObjCategory>("Bytes", ObjCategory::Bytes),
+		std::pair<std::string, ObjCategory>("Bytes", ObjCategory::Bytes)
+	);
+
+	const auto& specs = l[0].AsBytes();
+	if (specs.size() != 2)
+	{
+		throw ParseError(
+			"CAT String's specs bytes should be exactly 2 byte",
+			pos);
+	}
+	if (GetCatIdFromByte(specs[0]) != CatId::String)
+	{
+		throw ParseError(
+			"The given RLP list is not in CAT String",
+			pos);
+	}
+}
+
+
+namespace Internal
+{
+	template<typename _StrObjType>
+	struct StrReadRawDataUtf8
+	{
+
+		_StrObjType operator()(
+			size_t,
+			const uint8_t* begin, const uint8_t* end)
+		{
+			_StrObjType res;
+			auto dest = std::back_inserter(res);
+
+			using InputIt = decltype(begin);
+			using OutputIt = decltype(dest);
+
+			Internal::Utf::UtfConvert(
+				Internal::Utf::Utf8ToCodePtOnce<InputIt>,
+				Internal::Utf::CodePtToUtf8Once<OutputIt>,
+				begin, end, dest);
+
+			return res;
+		// LCOV_EXCL_START
+		}
+		// LCOV_EXCL_STOP
+
+	}; // struct StrReadRawDataUtf8
+} // namespace Internal
+
+
+template<
+	typename _ListObjType,
+	typename _StrObjType>
+struct TransformCatString
+{
+	using RetType = _StrObjType;
+
+
+	RetType operator()(size_t pos, _ListObjType&& l)
+	{
+		PreCheckCatString(pos, l);
+
+		// 1. char width
+		const auto& specs = l[0].AsBytes();
+		auto chWidth = specs[1];
+
+		// 2. raw data
+		const auto& rawData = l[1].AsBytes();
+		const uint8_t* rawDataBegin = rawData.data();
+		const uint8_t* rawDataEnd = rawData.data() + rawData.size();
+
+		switch (chWidth)
+		{
+		case 0x00U:
+			return Internal::StrReadRawDataUtf8<RetType>()(
+				pos,
+				rawDataBegin, rawDataEnd);
+
+		default:
+		{
+			std::string chWidthHex;
+			Internal::SimRlp::Internal::Obj::Internal::
+				ByteToString<std::string::value_type>(
+					std::back_inserter(chWidthHex), chWidth);
+
+			throw ParseError(
+				"The given char width value is unknown - " + chWidthHex, pos);
+		}
+		}
+	}
+}; // struct TransformCatString
+
+
+using CatStringParser = Internal::SimRlp::ListParserImpl<
+	Internal::SimRlp::InputContainerType,
+	Internal::SimRlp::ByteValType,
+	Internal::SimRlp::ListObjType,
+	TransformCatString<
+		Internal::SimRlp::ListObjType,
+		Internal::SimRlp::Internal::Obj::String>,
+	Internal::SimRlp::BytesParser,
+	Internal::SimRlp::SelfParserPlaceholder>;
+
 // ====================
 // Writers
 // ====================
+
+
+template<
+	typename _InObjType,
+	typename _OutCtnType>
+struct CatStringWriterImpl
+{
+	using Self = CatStringWriterImpl<_InObjType, _OutCtnType>;
+
+	using Concatenator = Internal::SimRlp::OutContainerConcat<_OutCtnType>;
+
+
+	inline static _OutCtnType Write(const _InObjType& val)
+	{
+		using _CharType = typename _InObjType::value_type;
+		size_t chWidth = sizeof(_CharType);
+
+		Concatenator ccntr;
+		_OutCtnType specs;
+		_OutCtnType rawData;
+
+		// 1.specs
+		specs.push_back(SerializeCatId(CatId::String));
+		specs.push_back(0x00U);
+		auto& chWidthByte = specs[1];
+
+		// 2.raw data
+		switch (chWidth)
+		{
+		case 1:
+		{
+			// 1-byte - assume to be UTF-8
+			chWidthByte = 0x00U;
+
+			auto begin = val.cbegin();
+			auto end = val.cend();
+			auto dest = std::back_inserter(rawData);
+
+			using InputIt = decltype(begin);
+			using OutputIt = decltype(dest);
+
+			Internal::Utf::UtfConvert(
+				Internal::Utf::Utf8ToCodePtOnce<InputIt>,
+				Internal::Utf::CodePtToUtf8Once<OutputIt>,
+				begin, end, dest);
+			break;
+		}
+
+		// LCOV_EXCL_START
+		default:
+			throw SerializeTypeError(
+				std::string(val.GetCategoryName()) + " that is not UTF-8",
+				"CatStringWriter");
+		// LCOV_EXCL_STOP
+		}
+
+		// 3.build RLP list
+		specs = Internal::SimRlp::
+			SerializeBytes(
+				Internal::SimRlp::RlpEncTypeCat::Bytes,
+				specs,
+				ccntr);
+		rawData = Internal::SimRlp::
+			SerializeBytes(
+				Internal::SimRlp::RlpEncTypeCat::Bytes,
+				rawData,
+				ccntr);
+		ccntr(specs, rawData);
+
+		return Internal::SimRlp::
+			SerializeBytes(
+				Internal::SimRlp::RlpEncTypeCat::List,
+				specs,
+				ccntr);
+	}
+
+}; // struct CatStringWriterImpl
+
+
+using CatStringWriter =
+	CatStringWriterImpl<
+		Internal::SimRlp::Internal::Obj::StringBaseObj,
+		Internal::SimRlp::OutputContainerType>;
+
 
 } // namespace AdvancedRlp
--- test/src/TestAdvRlpCatString.cpp
+++ test/src/TestAdvRlpCatString.cpp
@@ -23,3 +23,217 @@ GTEST_TEST(TestAdvRlpCatString, CountTestFile)
 	static auto tmp = ++SimpleRlp_Test::g_numOfTestFile;
 	(void)tmp;
 }
+
+GTEST_TEST(TestAdvRlpCatString, Transform)
+{
+	using TF =
+		TransformCatString<
+			SimpleObjects::List,
+			SimpleObjects::String>;
+
+	// Correct parsing
+	{
+		SimpleObjects::List testList = {
+			SimpleObjects::Bytes({ 0x08U, 0x00U, }),
+			SimpleObjects::Bytes({
+				0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U,
+				0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U, 0xE2U, 0x9CU, 0x85U, }),
+		};
+		SimpleObjects::String expOut =
+			"\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 \xE2\x9C\x85";
+
+		auto output = TF()(0, std::move(testList));
+		EXPECT_EQ(output, expOut);
+	}
+
+	// In correct num of items in list
+	{
+		SimpleObjects::List testList = {
+			SimpleObjects::Bytes({ 0x08U, 0x00U, }),
+			SimpleObjects::Bytes({ 0x20U, }),
+			SimpleObjects::Bytes({ 0x20U, }),
+		};
+
+		EXPECT_THROW(
+			TF()(0, std::move(testList)),
+			ParseError);
+	}
+
+	// In correct specs size
+	{
+		SimpleObjects::List testList = {
+			SimpleObjects::Bytes({ 0x08U, 0x00U,
+				0x01U, 0x00U, 0x00U, 0x00U, 0x00U, 0x00U, 0x00U, 0x00U, }),
+			SimpleObjects::Bytes({ 0x20U, }),
+		};
+
+		EXPECT_THROW(
+			TF()(0, std::move(testList)),
+			ParseError);
+	}
+
+	// In correct cat ID
+	{
+		SimpleObjects::List testList = {
+			SimpleObjects::Bytes({ 0x00U, 0x00U, }),
+			SimpleObjects::Bytes({ 0x20U, }),
+		};
+
+		EXPECT_THROW(
+			TF()(0, std::move(testList)),
+			ParseError);
+	}
+}
+
+GTEST_TEST(TestAdvRlpCatString, Parser)
+{
+
+	// Correct
+	{
+		auto output = CatStringParser().Parse(
+			std::vector<uint8_t>(
+				{
+					0xD1U,
+						0x82U,
+							0x08U, 0x00U,
+						0x8DU,
+							0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U,
+							0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U, 0xE2U, 0x9CU, 0x85U,
+				}));
+		auto expOut =
+			SimpleObjects::String("\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 \xE2\x9C\x85");
+
+		EXPECT_EQ(output, expOut);
+	}
+
+	// Correct encoding - extra raw data is a long byte
+	{
+		auto output = CatStringParser().Parse(
+			std::vector<uint8_t>({
+				0xF8U, 0x41U,
+					0x82U,
+						0x08U, 0x00U,
+					0xB8U, 0x3CU,
+						0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+						0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+						0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+						0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+						0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+						0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+			}));
+		auto expOut =
+			SimpleObjects::String(
+				"\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 "
+				"\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 "
+				"\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 "
+				"\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 "
+				"\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 "
+				"\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 "
+			);
+
+		EXPECT_EQ(output, expOut);
+	}
+
+	// Incorrect encoding - unknown char width byte
+	{
+		auto testProg = [](){
+			CatStringParser().Parse(
+				std::vector<uint8_t>({
+					0xD1U,
+						0x82U,
+							0x08U, 0xFFU,
+						0x8DU,
+							0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U,
+							0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U, 0xE2U, 0x9CU, 0x85U,
+				}));
+		};
+		EXPECT_THROW(testProg(), ParseError);
+	}
+
+	// Incorrect encoding - no raw data
+	{
+		auto testProg = [](){
+			CatStringParser().Parse(
+				std::vector<uint8_t>({
+					0xC3U,
+						0x82U,
+							0x08U, 0x00U,
+				}));
+		};
+		EXPECT_THROW(testProg(), ParseError);
+	}
+
+	// Incorrect encoding - extra raw data is a list
+	{
+		auto testProg = [](){
+			CatStringParser().Parse(
+				std::vector<uint8_t>({
+					0xC5U,
+						0x82U,
+							0x08U, 0x00U,
+						0xC1U,
+							0x20U,
+				}));
+		};
+		EXPECT_THROW(testProg(), ParseError);
+	}
+}
+
+GTEST_TEST(TestAdvRlpCatString, Writer)
+{
+	// Correct
+	{
+		auto input =
+			SimpleObjects::String("\xF0\x9F\x98\x82 \xF0\x9F\x98\x86 \xE2\x9C\x85");
+		auto expOut = std::vector<uint8_t>(
+		{
+			0xD1U,
+				0x82U,
+					0x08U, 0x00U,
+				0x8DU,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U,
+					0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U, 0xE2U, 0x9CU, 0x85U,
+		});
+
+		auto actOut = CatStringWriter::Write(input);
+		EXPECT_EQ(expOut, actOut);
+	}
+}
+
+GTEST_TEST(TestAdvRlpCatString, Consistency)
+{
+	{
+		auto inputAdvRlp = std::vector<uint8_t>(
+		{
+			0xD1U,
+				0x82U,
+					0x08U, 0x00U,
+				0x8DU,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U,
+					0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U, 0xE2U, 0x9CU, 0x85U,
+		});
+		auto decoded = CatStringParser().Parse(inputAdvRlp);
+		auto encoded = CatStringWriter::Write(decoded);
+
+		EXPECT_EQ(encoded, inputAdvRlp);
+	}
+	{
+		auto inputAdvRlp = std::vector<uint8_t>(
+		{
+			0xF8U, 0x41U,
+				0x82U,
+					0x08U, 0x00U,
+				0xB8U, 0x3CU,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+					0xF0U, 0x9FU, 0x98U, 0x82U, 0x20U, 0xF0U, 0x9FU, 0x98U, 0x86U, 0x20U,
+		});
+		auto decoded = CatStringParser().Parse(inputAdvRlp);
+		auto encoded = CatStringWriter::Write(decoded);
+
+		EXPECT_EQ(encoded, inputAdvRlp);
+	}
+}
